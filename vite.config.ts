29fed96f63a773e6f8406c962a3e0c762c4bf001
pranay <pranay@s2pedutech.com>
2026-04-04 14:50:22 +0530

    Added Commited Configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
@@ -22,7 +22,7 @@ export default defineConfig(({mode}) => {
       proxy: {
         // Forward all /api requests to the Node.js backend during local development
         '/api': {
-          target: 'http://localhost:5001',
+          target: 'https://quizmasterproserver-1.onrender.com/',
           changeOrigin: true,
         },
       },
